8ab010ca972319eaebf5221cf4f577717b151c30
Janosch Maier <maierjanosch@gmail.com>
2016-01-11 16:25:09 +0100

    First running dashboard

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# csd
+# Cyber Security Dashboard
+
 Cyber Security Dashboard for my Master's Thesis at TUM
+
+## Data Input
+
+Beware that you will need to get your data from somewhere.
+
+Currently the data is provided by an function that looks like this:
+
+```javascript
+function getData() {
+    return 'month,total,attack1,attack2\n' +
+    'Jan 2016,7,5,2\n' +
+    'Feb 2016,3,3,0\n';
+}
+```
---
+++ js/dashboard_chartist.js
@@ -0,0 +1,137 @@
+$( document ).ready(function() {
+
+    /* Helper Functions */
+
+    /**
+     * Remove the grid from a chart
+     * @param data
+     */
+    var removeGrid = function(data) {
+        if(data.type === 'grid' && data.index !== 0) {
+            data.element.remove();
+        }
+    }
+
+    /* Prepare Data */
+
+    var csv = getData();
+    var data = $.csv.toObjects(csv);
+
+    console.log(data);
+
+    attackTypes = ['Phishing', 'Information leakage', 'Injection attacks' ,'Malicious code',
+        'Ransomware/Cryptoware', 'Denial of service', 'Botnets', 'Cyber espionage',
+        'Data breaches', 'Hacking/Cracking', 'Overige', 'Anders', 'Niet van toepassing',
+        'Spam', 'Illegal content']
+
+    var monthLabels = [];
+    var attacksTotal = [];
+    var attacksNumbers = [];
+
+    for(attack of attackTypes) {
+        attacksNumbers[attack] = [];
+    }
+
+    var attacksPhishing = [];
+    var attacksInformationLeakage = [];
+    var attacksInjectionAttacks = [];
+
+    console.log(attacksNumbers);
+
+    for (obj of data) {
+        console.log(obj);
+        monthLabels.push(obj['month']);
+        attacksTotal.push(obj['total']);
+        attacksPhishing.push(obj['Phishing']);
+        attacksInformationLeakage.push(obj['Information leakage']);
+
+        attacksInjectionAttacks.push(obj['Injection attacks']);
+
+        for(attackType of attackTypes) {
+            attacksNumbers[attackType].push(obj[attackType]);
+        }
+
+    }
+
+    console.log(monthLabels);
+
+
+    /* Configurations */
+
+    var configBig = {
+        lineSmooth: false,
+        axisY: {
+            type: Chartist.AutoScaleAxis,
+            low: 0,
+            high: 100,
+            onlyInteger: true
+        }
+    };
+
+    var configMini = {
+        lineSmooth: false,
+        axisY: {
+            type: Chartist.AutoScaleAxis,
+            low: 0,
+            high: 30,
+            onlyInteger: true,
+            showGrid: false,
+        },
+        axisX: {
+            showLabel: false,
+            offset: 0,
+        }
+    };
+
+    var configMiniLast = {
+        lineSmooth: false,
+        axisY: {
+            type: Chartist.AutoScaleAxis,
+            low: 0,
+            high: 30,
+            onlyInteger: true
+        },
+    };
+
+
+    /* Create Charts */
+
+    var charts = [];
+
+    var chartIncidents = new Chartist.Line('#chart-incidents', {
+        labels: monthLabels,
+        series: [attacksTotal],
+    }, configBig);
+    charts.push(chartIncidents);
+
+    var chartPhishing = new Chartist.Line('#chart-phishing', {
+        labels: monthLabels,
+        series: [attacksNumbers['Phishing']],
+    }, configMini);
+    charts.push(chartPhishing);
+
+    var chartInformationLeakage = new Chartist.Line('#chart-information-leakage', {
+        labels: monthLabels,
+        series: [attacksNumbers['Information leakage']],
+    }, configMini);
+    charts.push(chartInformationLeakage);
+
+    var chartInjectionAttacks = new Chartist.Line('#chart-injection-attacks', {
+        labels: monthLabels,
+        series: [attacksNumbers['Injection attacks']],
+    }, configMini);
+    charts.push(chartInjectionAttacks)
+
+    var chartMaliciousCode = new Chartist.Line('#chart-malicious-code', {
+        labels: monthLabels,
+        series: [attacksNumbers['Malicious code']],
+    }, configMini);
+    charts.push(chartMaliciousCode);
+
+    // Remove Grid from all charts
+    for(chart of charts) {
+        chart.on('draw', removeGrid);
+    }
+
+
+});
